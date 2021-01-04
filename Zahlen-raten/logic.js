fe2b87e670a723e69bc282ff933c44182eba5fb9
cr542 <christian.reisenauer@gmx.at>
2021-01-04 15:54:19 +0100

    Abgabe

--- Zahlen-raten/logic.js
+++ Zahlen-raten/logic.js
@@ -10,29 +10,35 @@ function getCeiling() {
 function getUserNumber() {
     userNumber = zahl.value;
     if(userNumber >= 0 && userNumber <= getCeiling()) {
+        usrNr.innerText = "Deine Nummer: "+userNumber;
         return userNumber;
+
     } else {
         console.log("[*] Invalid number! Using standard value: 33");
+        usrNr.innerText = "Deine Nummer: 33 (Standardwert aufgrund ungültiger Eingabe)";
         return 33;
     }
 }
 
 function getRounds() {
     rounds = versuche.value;
-    if(!rounds > 5) {
-        return rounds;
-    } else {
+    if(rounds > 5) {
         console.log("[*] Rounds over 5");
         console.log("[*] Using standard value: 3");
+        rds.innerText = "Rundenzahl: 3 (Standardwert aufgrund ungültiger Eingabe)";
         return 3;
+    } else {
+        rds.innerText = "Rundenzahl: "+rounds;
+        return rounds;
     }
+
 }
 
 function createRandomNr() {
     let nr = Math.floor(Math.random() * getCeiling())
     console.log("[*] Created random number: "+ nr)
     // screen.innerHTML = "[*] Die generierte Zahl für diese Runde ist: "+ nr;
-    compNr.innerText = nr;
+    compNr.innerText = "Nummer des Computers: "+nr;
     return nr;
 }
 
@@ -40,11 +46,9 @@ function play() {
     let anzahlRunden = getRounds();
     let usrNumber = getUserNumber();
     console.log("[*] Users choice: " + userNumber);
-    // screen.innerHTML = "Test"
 
     let currentRound = 0;
     do {
-
         if(usrNumber == createRandomNr()) {
             console.log("[+] Success! User input equals the random value.");
             screen.innerHTML = "[+] Gratuliere! Du hast gewonnen!";
